ba24300d2a63d6b1da194c05941f63fe24b04687
artem-p <artempugachev1@gmail.com>
2018-05-07 17:16:33 +0300

    add methods to mvp contract

---
+++ app/src/main/java/ru/artempugachev/letsmvp/login/LoginMvpContract.kt
@@ -0,0 +1,32 @@
+package ru.artempugachev.letsmvp.login
+
+import ru.artempugachev.letsmvp.data.User
+
+interface LoginMvpContract {
+    interface View {
+        fun getFirstName(): String
+        fun getLastName(): String
+
+        fun showUserNotAvailable()
+        fun showInputError()
+        fun showUserSavedMessage()
+
+        fun setFirstName(firstName: String)
+        fun setLastName(lastName: String)
+    }
+
+
+    interface Presenter {
+        fun bindView(view: LoginMvpContract.View)
+
+        fun loginButtonClicked()
+        fun getCurrentUser()
+    }
+
+
+    interface Model {
+        fun createUser(firstName: String, lastName: String)
+
+        fun getUser(): User
+    }
+}
