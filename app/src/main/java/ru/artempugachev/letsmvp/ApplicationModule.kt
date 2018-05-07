604b331bb6ebee814901615ab7d904e87b42d9fb
artem-p <artempugachev1@gmail.com>
2018-05-07 12:03:12 +0300

    add application module

---
+++ app/src/main/java/ru/artempugachev/letsmvp/ApplicationModule.kt
@@ -0,0 +1,17 @@
+package ru.artempugachev.letsmvp
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApplicationModule(private val application: Application) {
+
+    @Provides
+    @Singleton
+    fun provideContext(): Context {
+        return application
+    }
+}
