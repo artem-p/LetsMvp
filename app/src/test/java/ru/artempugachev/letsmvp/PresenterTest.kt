8de146aeaa41bdc11f7f67b6966b55ee7e336420
artem-p <artempugachev1@gmail.com>
2018-05-10 12:51:34 +0300

    add tests to verify loading user from model

--- app/src/test/java/ru/artempugachev/letsmvp/PresenterTest.kt
+++ app/src/test/java/ru/artempugachev/letsmvp/PresenterTest.kt
@@ -1,6 +1,8 @@
 package ru.artempugachev.letsmvp
 
 import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.*
 import ru.artempugachev.letsmvp.data.User
 import ru.artempugachev.letsmvp.login.LoginMvpContract
 import ru.artempugachev.letsmvp.login.LoginPresenterImpl
@@ -16,6 +18,51 @@ class PresenterTest {
 
     @Before
     fun setUp() {
+        mockLoginModel = mock(LoginMvpContract.Model::class.java)
 
+        user = User("John", "Smith")
+
+
+
+        mockView = mock(LoginMvpContract.View::class.java)
+
+        presenter = LoginPresenterImpl(mockLoginModel)
+
+        presenter.bindView(mockView)
+    }
+
+
+    @Test
+    fun shouldShowUserWhenValidUserIsPresent() {
+        `when`(mockLoginModel.getUser()).thenReturn(user)
+
+        presenter.getCurrentUser()
+
+        // verify getting user from model
+        verify(mockLoginModel, times(1)).getUser()
+
+        // verify updating ui
+        // verify methods for setting name called exact one time
+        // and not calling method showUserNotAvailable
+        // That is to say - when an actual user object is found - the ui should be updated
+        // and no error should be shown
+        verify(mockView, times(1)).setFirstName("John")
+        verify(mockView, times(1)).setLastName("Smith")
+        verify(mockView, never()).showUserNotAvailable()
+    }
+
+
+    @Test
+    fun shouldShowErrorMessageWhenUserIsNull() {
+        `when`(mockLoginModel.getUser()).thenReturn(null)
+
+        presenter.getCurrentUser()
+
+        // verify getting user from model
+        verify(mockLoginModel, times(1)).getUser()
+
+        verify(mockView, never()).setFirstName("John")
+        verify(mockView, never()).setLastName("Smith")
+        verify(mockView, times(1)).showUserNotAvailable()
     }
 }
