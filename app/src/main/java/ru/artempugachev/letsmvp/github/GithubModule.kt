ca2250864260c227160d117a683b32e20cca09f7
artem-p <artempugachev1@gmail.com>
2018-05-11 14:07:52 +0300

    implement github module

--- app/src/main/java/ru/artempugachev/letsmvp/github/GithubModule.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/github/GithubModule.kt
@@ -1,7 +1,42 @@
 package ru.artempugachev.letsmvp.github
 
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.artempugachev.letsmvp.github.api.GithubInterface
 
 @Module
 class GithubModule {
+
+    /**
+     * Add logging to client
+     * */
+    @Provides
+    fun provideClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+
+    @Provides
+    fun provideRetrofit(baseUrl: String, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+
+
+    @Provides
+    fun provideApiService(): GithubInterface {
+        return provideRetrofit(BASE_URL, provideClient()).create(GithubInterface::class.java)
+    }
 }
+
+const val BASE_URL = "https://api.github.com"
--- app/src/main/java/ru/artempugachev/letsmvp/github/api/GithubInterface.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/github/api/GithubInterface.kt
@@ -4,7 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ApiInterface {
+interface GithubInterface {
     @GET("users")
     fun getUsers(@Query("per_page") usersPerPage: Int): Call<UserResponse>
 }
