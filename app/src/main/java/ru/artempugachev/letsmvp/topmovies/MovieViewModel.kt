e2014ca9c0437019b1e89427cc102a2cbbdc7b5b
artem-p <artempugachev1@gmail.com>
2018-05-15 13:49:37 +0300

    implement movies adapter

--- app/src/main/java/ru/artempugachev/letsmvp/topmovies/MovieViewModel.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/MovieViewModel.kt
@@ -5,6 +5,4 @@ package ru.artempugachev.letsmvp.topmovies
  * Defines model for elements displayed in list item.
  * Just movie name and country name
  * */
-class MovieViewModel {
-
-}
+data class MovieViewModel(val name: String, val country: String)
--- app/src/main/java/ru/artempugachev/letsmvp/topmovies/MoviesAdapter.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/MoviesAdapter.kt
@@ -1,26 +1,55 @@
 package ru.artempugachev.letsmvp.topmovies
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.top_movies_list_item.view.*
+import ru.artempugachev.letsmvp.R
 
 class MoviesAdapter: RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+    private var movies: List<MovieViewModel>
+
+    init {
+        this.movies = ArrayList()
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val context = parent.context
+        val categoryItemLayoutId = R.layout.top_movies_list_item
+        val inflater = LayoutInflater.from(context)
+        val shouldAttachToParentImmediately = false
+
+        val view = inflater.inflate(categoryItemLayoutId, parent, shouldAttachToParentImmediately)
+
+        return MoviesViewHolder(view)
     }
 
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return movies.size
     }
 
 
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (!movies.isEmpty()) {
+            val movie = movies[position]
+            holder.movieName.text = movie.name
+            holder.countryName.text = movie.country
+        }
+    }
+
+
+    fun swap(newMovies: List<MovieViewModel>) {
+        this.movies = newMovies
+        notifyDataSetChanged()
     }
 
 
     inner class MoviesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val movieName: TextView = itemView.movieName
+        val countryName: TextView = itemView.countryName
     }
 }
