3f65e8d7a42a62cb37de9c7ffbc4b0abae6ba31e
artem-p <artempugachev1@gmail.com>
2018-05-15 11:41:33 +0300

    add top movies activity

---
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/TopMoviesActivity.kt
@@ -0,0 +1,13 @@
+package ru.artempugachev.letsmvp.topmovies
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.artempugachev.letsmvp.R
+
+class TopMoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_movies)
+    }
+}
