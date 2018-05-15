1206d5cc2820f13f7b49f98ff7daedcdfa9f9329
artem-p <artempugachev1@gmail.com>
2018-05-15 13:05:02 +0300

    add adapter stub

---
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/MoviesAdapter.kt
@@ -0,0 +1,26 @@
+package ru.artempugachev.letsmvp.topmovies
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class MoviesAdapter: RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    inner class MoviesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
--- app/src/main/java/ru/artempugachev/letsmvp/topmovies/TopMoviesActivity.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/TopMoviesActivity.kt
@@ -2,11 +2,23 @@ package ru.artempugachev.letsmvp.topmovies
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListAdapter
+import kotlinx.android.synthetic.main.activity_top_movies.*
 import ru.artempugachev.letsmvp.R
 
 class TopMoviesActivity : AppCompatActivity() {
+    private lateinit var adapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_movies)
+
+        setUpViews()
+    }
+
+
+    private fun setUpViews() {
+        adapter = MoviesAdapter()
+        moviesRecycler.adapter = adapter
     }
 }
