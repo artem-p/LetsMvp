cdda0f645c862e7ae43c52c0d37dfe2356898b6e
artem-p <artempugachev1@gmail.com>
2018-05-16 13:31:56 +0300

    add repository interface

---
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/Repository.kt
@@ -0,0 +1,18 @@
+package ru.artempugachev.letsmvp.topmovies
+
+import io.reactivex.Observable
+import ru.artempugachev.letsmvp.topmovies.api.MovieDbResponse
+
+interface Repository {
+    fun getMoviesFromMemory(): Observable<MovieDbResponse>
+
+    fun getMoviesFromNetwork(): Observable<MovieDbResponse>
+
+    fun getCountriesFromMemory(): Observable<String>
+
+    fun getCountriesFromNetwork(): Observable<String>
+
+    fun getCountryData(): Observable<String>
+
+    fun getResultData(): Observable<MovieDbResponse>
+}
