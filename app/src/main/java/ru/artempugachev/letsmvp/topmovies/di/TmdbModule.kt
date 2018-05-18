5394d35c985a330b45d9aeaa49c28b4df018036d
artem-p <artempugachev1@gmail.com>
2018-05-18 10:27:18 +0300

    comment

--- app/src/main/java/ru/artempugachev/letsmvp/topmovies/di/TmdbModule.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/di/TmdbModule.kt
@@ -9,7 +9,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.artempugachev.letsmvp.topmovies.api.TmdbService
 
-
+/**
+ * Provide Retrofit api interface for Tmdb
+ * */
 class TmdbModule {
     @Provides
     private fun provideClient(): OkHttpClient {
