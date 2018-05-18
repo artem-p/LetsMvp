de84edceaf9680a30441a3ed0750659561e809a7
artem-p <artempugachev1@gmail.com>
2018-05-18 12:39:35 +0300

    add fields to the repository implementation

--- app/src/main/java/ru/artempugachev/letsmvp/topmovies/RepositoryImpl.kt
+++ app/src/main/java/ru/artempugachev/letsmvp/topmovies/RepositoryImpl.kt
@@ -7,6 +7,13 @@ import ru.artempugachev.letsmvp.topmovies.api.TmdbService
 
 class RepositoryImpl(private val tmdbService: TmdbService,
                      private val omdbService: OmdbService) : Repository {
+
+    private var lastUpdateTime = 0
+    private var movies = ArrayList<TmdbMovie>()
+    private var countries = ArrayList<String>()
+
+
+
     override fun getMoviesFromMemory(): Observable<TmdbMovie> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -31,4 +38,8 @@ class RepositoryImpl(private val tmdbService: TmdbService,
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+
+    companion object {
+        const val VALID_TIME = 10 * 1000    // time after cache expired
+    }
 }
